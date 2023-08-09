0b4752a3ee9ceec27497aa803dd93a14c7582afe
pkatkade <43074147+pkatkade@users.noreply.github.com>
2023-08-09 21:25:15 +0530

    Add files via upload

---
+++ Body.js
@@ -0,0 +1,540 @@
+
+
+
+export  const Burgerking = [{
+    cardType: "restaurant",
+    layoutAlignmentType: "VERTICAL",
+    data: {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "25547",
+        name: "Narayan Sandwich House",
+        "uuid": "6c22c873-6537-4ac0-9746-7238e2805c6e",
+        "city": "5",
+        "area": "Andheri East",
+        "totalRatingsString": "",
+        "cloudinaryImageId": "d58aj6t7jorlzr7cidds",
+        cuisines: [
+          "Healthy Food",
+          "Snacks"
+        ],
+        "tags": [
+          
+        ],
+        "costForTwo": 25000,
+        "costForTwoString": "₹250 FOR TWO",
+        "deliveryTime": 50,
+        "minDeliveryTime": 50,
+        "maxDeliveryTime": 50,
+        "slaString": "50 MINS",
+        "lastMileTravel": 0,
+        "slugs": {
+          "restaurant": "narayan-sandwich-house-marol-naka-marol-sakinaka",
+          "city": "Mumbai"
+        },
+        "cityState": "5",
+        "address": "",
+        "locality": "Marol Naka",
+        "parentId": 144193,
+        "unserviceable": false,
+        "veg": true,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+          
+        ],
+        "aggregatedDiscountInfoV2": {
+          "header": "",
+          "shortDescriptionList": [
+            
+          ],
+          "descriptionList": [
+            
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [
+          
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 4700,
+              "message": ""
+            },
+            {
+              "name": "BASE_TIME",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 4700,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "4700",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=7649033~p=1~eid=00000189-af0a-3dc1-014f-d6a600850100~srvts=1690858831297~83667",
+        "badges": {
+          "imageBased": [
+            
+          ],
+          "textBased": [
+            
+          ],
+          "textExtendedBadges": [
+            
+          ]
+        },
+        "lastMileTravelString": "4.8 kms",
+        "hasSurge": false,
+        "cta": {
+          "link": "swiggy://menu?restaurant_id=25547&source=collection&query=Sandwich",
+          "type": "DEEPLINK",
+          "text": "RESTAURANT_MENU"
+        },
+        "sla": {
+          "restaurantId": "",
+          "deliveryTime": 50,
+          "minDeliveryTime": 50,
+          "maxDeliveryTime": 50,
+          "lastMileTravel": 0,
+          "lastMileDistance": 4.8,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "4.2",
+        "totalRatings": 0,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    "parentWidget": false
+  },
+  {
+    "cardType": "restaurant",
+    "layoutAlignmentType": "VERTICAL",
+    "data": {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "460415",
+        "name": "Chaayos Chai+Snacks=Relax",
+        "uuid": "242ad7b3-ef59-4d10-b99d-9ba709a04230",
+        "city": "5",
+        "area": "kurla",
+        "totalRatingsString": "",
+        "cloudinaryImageId": "2beebd3688e63e027ee8a11c1c8a72c8",
+        "cuisines": [
+          "Bakery",
+          "Beverages",
+          "Chaat",
+          "Desserts",
+          "Home Food",
+          "Italian",
+          "Maharashtrian",
+          "Snacks",
+          "Street Food",
+          "Sweets"
+        ],
+        "tags": [
+          
+        ],
+        "costForTwo": 25000,
+        "costForTwoString": "₹250 FOR TWO",
+        "deliveryTime": 43,
+        "minDeliveryTime": 43,
+        "maxDeliveryTime": 43,
+        "slaString": "43 MINS",
+        "lastMileTravel": 0,
+        "slugs": {
+          "restaurant": "chaayos-meri-wali-chai-ghatkopar-ck-mum-kurla",
+          "city": "Mumbai"
+        },
+        "cityState": "5",
+        "address": "",
+        "locality": "Ghatkopar CK Mum",
+        "parentId": 281782,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+          
+        ],
+        "aggregatedDiscountInfoV2": {
+          "header": "",
+          "shortDescriptionList": [
+            
+          ],
+          "descriptionList": [
+            
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [
+          
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3300,
+              "message": ""
+            },
+            {
+              "name": "BASE_TIME",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3300,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3300",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [
+            
+          ],
+          "textBased": [
+            
+          ],
+          "textExtendedBadges": [
+            
+          ]
+        },
+        "lastMileTravelString": "2.7 kms",
+        "hasSurge": false,
+        "cta": {
+          "link": "swiggy://menu?restaurant_id=460415&source=collection&query=Sandwich",
+          "type": "DEEPLINK",
+          "text": "RESTAURANT_MENU"
+        },
+        "sla": {
+          "restaurantId": "",
+          "deliveryTime": 43,
+          "minDeliveryTime": 43,
+          "maxDeliveryTime": 43,
+          "lastMileTravel": 0,
+          "lastMileDistance": 2.7,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "4.5",
+        "totalRatings": 0,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    "parentWidget": false
+  },
+  {
+    "cardType": "restaurant",
+    "layoutAlignmentType": "VERTICAL",
+    "data": {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "31775",
+        "name": "Subway",
+        "uuid": "4939f56c-9f76-4021-8ebe-5476f1775355",
+        "city": "5",
+        "area": "Bandra East",
+        "totalRatingsString": "",
+        "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+        "cuisines": [
+          "Salads",
+          "Snacks",
+          "Desserts",
+          "Beverages"
+        ],
+        "tags": [
+          
+        ],
+        "costForTwo": 35000,
+        "costForTwoString": "₹350 FOR TWO",
+        "deliveryTime": 33,
+        "minDeliveryTime": 33,
+        "maxDeliveryTime": 33,
+        "slaString": "33 MINS",
+        "lastMileTravel": 0,
+        "slugs": {
+          "restaurant": "subways-bandra-kurla-complex-scruz-bandra-east",
+          "city": "Mumbai"
+        },
+        "cityState": "5",
+        "address": "",
+        "locality": "The Capital Bandra Kurla Complex",
+        "parentId": 2,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+          
+        ],
+        "aggregatedDiscountInfoV2": {
+          "header": "",
+          "shortDescriptionList": [
+            
+          ],
+          "descriptionList": [
+            
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [
+          
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3300,
+              "message": ""
+            },
+            {
+              "name": "BASE_TIME",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3300,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3300",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [
+            
+          ],
+          "textBased": [
+            
+          ],
+          "textExtendedBadges": [
+            
+          ]
+        },
+        "lastMileTravelString": "2.6 kms",
+        "hasSurge": false,
+        "cta": {
+          "link": "swiggy://menu?restaurant_id=31775&source=collection&query=Sandwich",
+          "type": "DEEPLINK",
+          "text": "RESTAURANT_MENU"
+        },
+        "sla": {
+          "restaurantId": "",
+          "deliveryTime": 33,
+          "minDeliveryTime": 33,
+          "maxDeliveryTime": 33,
+          "lastMileTravel": 0,
+          "lastMileDistance": 2.6,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "4.0",
+        "totalRatings": 0,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    "parentWidget": false
+  },
+  {
+    "cardType": "restaurant",
+    "layoutAlignmentType": "VERTICAL",
+    "data": {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "14744",
+        "name": "The Bagel Shop",
+        "uuid": "acadb7b8-6435-4c22-bda4-d7f4a459064a",
+        "city": "5",
+        "area": "Bandra West",
+        "totalRatingsString": "",
+        "cloudinaryImageId": "lkal9rlydk3cunkqwr35",
+        "cuisines": [
+          "American",
+          "Salads",
+          "Juices",
+          "Desserts"
+        ],
+        "tags": [
+          
+        ],
+        "costForTwo": 85000,
+        "costForTwoString": "₹850 FOR TWO",
+        "deliveryTime": 47,
+        "minDeliveryTime": 47,
+        "maxDeliveryTime": 47,
+        "slaString": "47 MINS",
+        "lastMileTravel": 0,
+        "slugs": {
+          "restaurant": "the-bagel-shop-carter-road-bandra-west-bandra-west",
+          "city": "Mumbai"
+        },
+        "cityState": "5",
+        "address": "",
+        "locality": "Bandra West",
+        "parentId": 204866,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+          
+        ],
+        "aggregatedDiscountInfoV2": {
+          "header": "",
+          "shortDescriptionList": [
+            
+          ],
+          "descriptionList": [
+            
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [
+          
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 6500,
+              "message": ""
+            },
+            {
+              "name": "BASE_TIME",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 6500,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "6500",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=7644723~p=2~eid=00000189-af0a-3dc1-014f-d6a70085023f~srvts=1690858831297~83667",
+        "badges": {
+          "imageBased": [
+            
+          ],
+          "textBased": [
+            
+          ],
+          "textExtendedBadges": [
+            
+          ]
+        },
+        "lastMileTravelString": "8 kms",
+        "hasSurge": false,
+        "cta": {
+          "link": "swiggy://menu?restaurant_id=14744&source=collection&query=Sandwich",
+          "type": "DEEPLINK",
+          "text": "RESTAURANT_MENU"
+        },
+        "sla": {
+          "restaurantId": "",
+          "deliveryTime": 47,
+          "minDeliveryTime": 47,
+          "maxDeliveryTime": 47,
+          "lastMileTravel": 0,
+          "lastMileDistance": 8,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "4.4",
+        "totalRatings": 0,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    "parentWidget": false
+  }
+    ]
